ba4cddc2b50a3fc8b0e450ee7777ff334386e6a9
Héctor Tovar <soyeduardoo20@gmail.com>
2025-08-02 22:47:51 -0600

    fix: update Expo prebuild check to specify Android platform and refactor layout component to use Tabs

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,14 +1,6 @@
-import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
 import "react-native-reanimated";
 
-export default function Layout() {
-  return (
-    <Stack>
-      <Stack.Screen name="chat" options={{ headerShown: false }} />
-      <Stack.Screen name="profile" options={{ headerShown: false }} />
-      <Stack.Screen name="books" options={{ headerShown: false }} />
-      <Stack.Screen name="home" options={{ headerShown: false }} />
-    </Stack>
-  );
-}
+const Layout = () => {
+  <Tabs></Tabs>;
+};
