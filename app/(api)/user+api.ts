21caaa157aa5285dc3ce634af6f326f6714c0ff7
Héctor Tovar <soyeduardoo20@gmail.com>
2025-08-03 17:39:13 -0600

    feat: implement user registration API and integrate with sign-up flow

---
+++ app/(api)/user+api.ts
@@ -0,0 +1,22 @@
+import { neon } from "@neondatabase/serverless";
+
+export async function POST(request: Request) {
+  const sql = neon(`${process.env.DATABASE_URL}`);
+  const { name, email, clerkId } = await request.json();
+
+  if (!name || !email || !clerkId) {
+    return Response.json({ error: "Missing required fields" }, { status: 400 });
+  }
+
+  try {
+    const response = await sql`
+            INSERT INTO users (name, email, clerk_id) VALUES (${name}, ${email}, ${clerkId})
+        `;
+    return new Response(JSON.stringify({ response }), { status: 201 });
+  } catch (err) {
+    return Response.json(
+      { error: "Database error", details: err },
+      { status: 500 }
+    );
+  }
+}
